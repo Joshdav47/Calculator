2c9224c433524596966f9b2d50e05349ac1b2e5e
Joshdav47 <167588957+Joshdav47@users.noreply.github.com>
2024-08-30 15:21:26 -0500

    Update

--- logic.py
+++ logic.py
@@ -7,3 +7,26 @@ class calc_logic(QMainWindow, Ui_UI_Calculator):
         Method to initialize the Calculator controls
         """
         super().__init__()
+        self.setupUi(self)
+
+        # Initialize the LCD display
+        self.lcdNumber.setDigitCount(10)  # Set the number of digits to display
+
+        # Connect number buttons
+        self.zero.clicked.connect(lambda: self.number_pressed('0'))
+        self.one.clicked.connect(lambda: self.number_pressed('1'))
+        self.two.clicked.connect(lambda: self.number_pressed('2'))
+        self.three.clicked.connect(lambda: self.number_pressed('3'))
+        self.four.clicked.connect(lambda: self.number_pressed('4'))
+        self.five.clicked.connect(lambda: self.number_pressed('5'))
+        self.six.clicked.connect(lambda: self.number_pressed('6'))
+        self.seven.clicked.connect(lambda: self.number_pressed('7'))
+        self.eight.clicked.connect(lambda: self.number_pressed('8'))
+        self.nine.clicked.connect(lambda: self.number_pressed('9'))
+
+        # Initialize the current value
+        self.current_value = ''
+
+    def number_pressed(self, number):
+        self.current_value += number
+        self.lcdNumber.display(self.current_value)
