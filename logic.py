950f55c79dd36979d5cd6231bc70e7d65fecd226
Joshdav47 <167588957+Joshdav47@users.noreply.github.com>
2024-08-31 16:53:07 -0500

    Update

--- logic.py
+++ logic.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import *
 from ui_calculator import *
+import math
 
 class calc_logic(QMainWindow, Ui_UI_Calculator):
     def __init__(self) -> None:
@@ -12,8 +13,11 @@ class calc_logic(QMainWindow, Ui_UI_Calculator):
         # Initialize the LCD display
         self.lcdNumber.setDigitCount(10)  # Set the number of digits to display
 
-        # Initialize the current value
+        # Initialize values
         self.current_value = ''
+        self.previous_value = ''
+        self.current_operator = None
+
 
         # Connect number buttons
         self.zero.clicked.connect(lambda: self.number_pressed('0'))
@@ -35,15 +39,121 @@ class calc_logic(QMainWindow, Ui_UI_Calculator):
         self.clear_entry.clicked.connect(self.clear_entry_display)
         self.delete_2.clicked.connect(self.delete)
         self.percentage.clicked.connect(self.percentage_conversion)
-        self.square.cliked.connect(self.square_x)
+        self.square.clicked.connect(self.square_x)
         self.square_root.clicked.connect(self.square_root_x)
         self.divide.clicked.connect(self.division)
-        self.one_div_x.connect.clicked(self.div_by_x)
-        self.multiply.connect.clicked(self.multiplication)
-        self.subtraction.connect.clicked.connect(self.subtract)
-        self.addition.connect.clicked.connect(self.add)
-        self.enter.connect.clicked.connect(self.display_answer)
-        self.positive_negative.connect.clicked.connect(self.interger_conversion)
+        self.one_div_x.clicked.connect(self.div_by_x)
+        self.multiply.clicked.connect(self.multiplication)
+        self.subtraction.clicked.connect(self.subtract)
+        self.addition.clicked.connect(self.add)
+        self.enter.clicked.connect(self.display_answer)
+        self.positive_negative.clicked.connect(self.integer_conversion)
+
+    def clear_entry_display(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def percentage_conversion(self):
+        pass
+
+    def square_x(self):
+        self.current_operator = 'square'
+        if self.current_value:
+            # Perform the addition
+            result = math.sqrt(float(self.current_value))
+            self.previous_value = str(result)
+            return result
+        elif self.current_value:
+            self.previous_value = self.current_value
+            self.current_value = ''
+            return None
+
+    def square_root_x(self):
+        pass
+
+    def division(self):
+        self.current_operator = 'divide'
+        if self.current_value and self.previous_value:
+            # Perform the addition
+            if self.current_value == '0':
+                return "ERROR"
+            else:
+                result = float(self.previous_value) / float(self.current_value)
+                self.previous_value = str(result)
+                return result
+        elif self.current_value:
+            self.previous_value = self.current_value
+            self.current_value = ''
+            return None
+
+    def div_by_x(self):
+        pass
+
+    def multiplication(self):
+        self.current_operator = 'multiply'
+        if self.current_value and self.previous_value:
+            # Perform the addition
+            result = float(self.previous_value) * float(self.current_value)
+            self.previous_value = str(result)
+            return result
+        elif self.current_value:
+            self.previous_value = self.current_value
+            self.current_value = ''
+            return None
+
+    def subtract(self):
+        self.current_operator = 'subtract'
+        if self.current_value and self.previous_value:
+            # Perform the addition
+            result = float(self.previous_value) - float(self.current_value)
+            self.previous_value = str(result)
+            return result
+        elif self.current_value:
+            self.previous_value = self.current_value
+            self.current_value = ''
+            return None
+
+    def add(self):
+            self.current_operator = 'add'
+            if self.current_value and self.previous_value:
+                    # Perform the addition
+                    result = float(self.previous_value) + float(self.current_value)
+                    self.previous_value = str(result)
+                    return result
+            elif self.current_value:
+                self.previous_value = self.current_value
+                self.current_value = ''
+                return None
+
+    def display_answer(self):
+        result = 0
+        if self.current_operator and self.previous_value and self.current_value:
+            if self.current_operator == 'add':
+                result = self.add()
+            elif self.current_operator == 'subtract':
+                result = self.subtract()
+            elif self.current_operator == 'multiply':
+                result = self.multiplication()
+            elif self.current_operator == 'divide':
+                result = self.division()
+            elif self.current_operator == 'square':
+                result = self.square_x()
+            elif self.current_operator == 'square_root':
+                result = self.square_root_x()
+            elif self.current_operator == 'div_by_x':
+                result = self.div_by_x()
+            elif self.current_operator == 'percentage':
+                result = self.percentage_conversion()
+
+            self.lcdNumber.display(str(result))
+            self.previous_value = str(result)
+            self.current_value = ''
+            self.current_operator = None
+
+    def integer_conversion(self):
+        pass
 
     def number_pressed(self, number):
         self.current_value += number
@@ -56,4 +166,6 @@ class calc_logic(QMainWindow, Ui_UI_Calculator):
 
     def clear_display(self):
         self.current_value = ''
+        self.previous_value = ''
+        self.current_operator = None
         self.lcdNumber.display('0')
